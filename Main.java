2bc2ffebbf59a9a14215031bd05ad39468d40e4c
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-02-21 03:07:16 +0300

    4.2

--- Main.java
+++ Main.java
@@ -3,19 +3,46 @@ package com.metanit;
 public class Main {
 
     public static void main(String[] args) {
-        Day current = Day.MONDAY;
-        System.out.println(current);    // MONDAY
+        Book b1 = new Book("War and Peace", "L. Tolstoy", Type.BELLETRE);
+        System.out.printf("Book '%s' has a type %s", b1.name, b1.bookType);
+
+        switch(b1.bookType){
+            case BELLETRE:
+                System.out.println("Belletre");
+                break;
+            case SCIENCE:
+                System.out.println("Science");
+                break;
+            case SCIENCE_FICTION:
+                System.out.println("Science fiction");
+                break;
+            case PHANTASY:
+                System.out.println("Phantasy");
+                break;
+        }
     }
 }
-enum Day{
+class Book{
 
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    String name;
+    Type bookType;
+    String author;
+
+    Book(String name, String author, Type type){
+
+        bookType = type;
+        this.name = name;
+        this.author = author;
+    }
+}
+
+enum Type
+{
+    SCIENCE,
+    BELLETRE,
+    PHANTASY,
+    SCIENCE_FICTION
+
 }
 
 
